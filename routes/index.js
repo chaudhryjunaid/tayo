2b478fbdcfb89a0fa5f0afdc8752cf7424057cde
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2019-11-23 20:51:48 +0500

    initial completed version for ts slash command

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.post('/ts', function(req, res, next) {
+router.post('/index', function(req, res, next) {
   res.send('hello, world');
 });
 

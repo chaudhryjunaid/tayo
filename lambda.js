1a87448cbd328ecbb5d88ff674c1331a8d744aea
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2019-11-25 18:18:02 +0500

    fix base64 error

--- lambda.js
+++ lambda.js
@@ -7,23 +7,13 @@ const app = require('./app');
 // by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
 // binaryMimeTypes below, then redeploy (`npm run package-deploy`)
 const binaryMimeTypes = [
-  'application/javascript',
-  'application/json',
   'application/octet-stream',
-  'application/xml',
   'font/eot',
   'font/opentype',
   'font/otf',
   'image/jpeg',
   'image/png',
-  'image/svg+xml',
-  'text/comma-separated-values',
-  'text/css',
-  'text/html',
-  'text/javascript',
-  'text/plain',
-  'text/text',
-  'text/xml'
+  'image/svg+xml'
 ];
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
 
